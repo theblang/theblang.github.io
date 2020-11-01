89e4226d8db1c54068acc3a6e8858a3332102e6d
Matt Langston <theblang@gmail.com>
2020-10-31 21:14:36 -0500

    Add a list of blog posts to the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,61 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
+import PostLink from '../components/post-link';
 
-const IndexPage = () => (
-    <Layout>
-        <SEO title="Home" />
-        <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <a href="https://stackoverflow.com/users/1747491/theblang">
-            <img
-                src="https://stackoverflow.com/users/flair/1747491.png?theme=dark"
-                width="208"
-                height="58"
-                alt="profile for theblang at Stack Overflow, Q&amp;A for professional and enthusiast programmers"
-                title="profile for theblang at Stack Overflow, Q&amp;A for professional and enthusiast programmers"
-            />
-        </a>
-        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-            <Image />
-        </div>
-        <Link to="/page-2/">Go to page 2</Link> <br />
-        <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-        <Link to="/blog/my-first-post">Testing blog post</Link>
-    </Layout>
-);
+const IndexPage = ({
+    data: {
+        allMarkdownRemark: { edges },
+    },
+}) => {
+    const Posts = edges
+        .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+        .map(edge => <PostLink key={edge.node.id} post={edge.node} />);
+
+    return (
+        <Layout>
+            <SEO title="Home" />
+            <h1>Hi people</h1>
+            <p>Welcome to your new Gatsby site.</p>
+            <p>Now go build something great.</p>
+            <a href="https://stackoverflow.com/users/1747491/theblang">
+                <img
+                    src="https://stackoverflow.com/users/flair/1747491.png?theme=dark"
+                    width="208"
+                    height="58"
+                    alt="profile for theblang at Stack Overflow, Q&amp;A for professional and enthusiast programmers"
+                    title="profile for theblang at Stack Overflow, Q&amp;A for professional and enthusiast programmers"
+                />
+            </a>
+            <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+                <Image />
+            </div>
+            <Link to="/page-2/">Go to page 2</Link> <br />
+            <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
+            <div>{Posts}</div>
+        </Layout>
+    );
+};
 
 export default IndexPage;
+
+export const pageQuery = graphql`
+    query {
+        allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+            edges {
+                node {
+                    id
+                    excerpt(pruneLength: 250)
+                    frontmatter {
+                        date(formatString: "MMMM DD, YYYY")
+                        slug
+                        title
+                    }
+                }
+            }
+        }
+    }
+`;
